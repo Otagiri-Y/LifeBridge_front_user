4de97cf5bead436a4f5b1ad6f1a1a995c36b510f
takuyayamaguchi <takuya.yamaguchi0522@gmail.com>
2025-04-21 21:51:02 +0900

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -219,7 +219,7 @@ export default function Home() {
 
         // FastAPIサーバーのURLを指定（認証トークン付き）
         const response = await fetch(`${API_BASE_URL}/api/user/search`, {
-          method: "POST", // メソッドを明示的にPOSTに指定
+          method: "GET", // メソッドを明示的にPOSTに指定
           headers: {
             Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
